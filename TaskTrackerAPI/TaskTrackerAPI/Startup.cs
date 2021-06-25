5596e220f569a974381cdd88e8b0dd068fb41f4a
Alex <der.lexus911@gmail.com>
2021-06-25 20:46:34 +0200

    Add Docker support
    Adds dockerfile and docker-compose files to the project.

--- TaskTrackerAPI/TaskTrackerAPI/Startup.cs
+++ TaskTrackerAPI/TaskTrackerAPI/Startup.cs
@@ -37,6 +37,13 @@ namespace TaskTrackerApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>()
+            .CreateScope())
+            {
+                serviceScope.ServiceProvider.GetService<DataContext>()
+                    .Database.Migrate();
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
